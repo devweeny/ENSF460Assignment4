2acc43812b94957154a94c7acca58e4a9356091d
Dylan <dylan.wenaas@ucalgary.ca>
2024-11-04 11:43:49 -0700

    ADC logic working

--- ADC.c
+++ ADC.c
@@ -10,33 +10,44 @@
 #include "ADC.h"
 #include "clkChange.h"
 
+void init_ADC() {
+    TRISAbits.TRISA3 = 1;
+    
+    AD1PCFG = 0xffff;
+    AD1CON1bits.SSRC = 0b111;
+    AD1CON1bits.FORM = 0;
+    AD1CON1bits.ASAM = 0;
+    AD1CON1bits.ADON = 1;
+    AD1CON1bits.DONE = 1;
+    AD1CON2bits.VCFG = 0b000;
+    AD1CON2bits.CSCNA = 0;
+    AD1CON2bits.SMPI = 0b0000;
+    AD1CON2bits.BUFM = 0;
+    AD1CON2bits.ALTS = 0;
+    AD1CON3bits.ADRC = 0;
+    AD1CON3bits.SAMC = 0b00111;
+    AD1CHSbits.CH0SA = 0b0101;
+    AD1CHSbits.CH0NA = 0;    
+    AD1PCFGbits.PCFG5 = 0;
+    AD1CSSLbits.CSSL5 = 0;
+}
+
 uint16_t do_ADC(void) {
     uint16_t ADCvalue;
-    
-    TRISAbits.TRISA3 = 1;
-    AD1PCFGbits.PCFG5 = 0;
-   
-    AD1CON2bits.VCFG = 0b000;
-    
-    AD1CON3bits.ADRC = 0;
-    
-    
+    init_ADC();
+
     AD1CON1bits.SAMP = 1;
-    AD1CON1bits.ADON = 1;
     
-    while (!AD1CON1bits.DONE) {}
+    while (AD1CON1bits.DONE == 0) {}
     ADCvalue = ADC1BUF0;
     
     AD1CON1bits.SAMP = 0;
-    AD1CON1bits.ADON = 0;
     
     return (ADCvalue);  
 }
 
 void IOinit() {
-       
-    AD1PCFG = 0xFFFF; /* keep this line as it sets I/O pins that can also be analog to be digital */
-    
+           
     newClk(500);
     
     //T3CON config
@@ -85,23 +96,33 @@ void IOinit() {
 void IOcheck() {
     
     if (tmr3_event) { // Clock interrupt to run once a second
-
-        Disp2String("\033[2J"); // Clears the terminal
-        Disp2String("\033[H"); // Sets cursor to top left of terminal
-        LATBbits.LATB8 ^= 1;
-
-//        uint16_t result = do_ADC();
-        uint16_t result = 0;
-        Disp2Hex(result);
         tmr3_event = 0;
+
+        uint16_t result = do_ADC();
+        
+        if (mode == 0) {
+            char bar[34] = "*";
+
+            int num = 32 * ((double) result / 0x3ff);
+            int i = 0;
+            for (i = 1; i < num + 1; i++) {
+                bar[i] = '*';
+            }
+            bar[i] = ' \0';
+            
+            Disp2String("\033[2J"); // Clears the terminal
+            Disp2String("\033[H"); // Sets cursor to top left of terminal
+            Disp2String("Mode 0: ");
+            Disp2String(bar);
+            Disp2Hex(result);
+        } else {
+            Disp2String("\r\n");
+            Disp2Hex(result);
+        }
     }
 
     if (PB1_event) {
-        Disp2String("\033[2J"); // Clears the terminal
-        Disp2String("\033[H"); // Sets cursor to top left of terminal
         mode ^= 1;
-        Disp2String("Mode: ");
-        Disp2Dec(mode);
         PB1_event = 0;
     }
     Idle();
